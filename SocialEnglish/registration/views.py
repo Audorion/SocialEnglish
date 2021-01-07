9c5293262ddc4b68fd85b2bafee753c707e72da6
Maksim <maksimov.mxv@gmail.com>
2021-01-07 03:47:55 +0300

    Add ru language, bootstrap addons, registration link, footer and head

--- SocialEnglish/registration/views.py
+++ SocialEnglish/registration/views.py
@@ -3,8 +3,8 @@ from django.urls import reverse_lazy
 from .forms import CreationForm
 
 
+# Register view
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
-
